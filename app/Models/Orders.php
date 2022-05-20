23e1a521e21412d3a5c516980a46e223d71beb93
Luiz Augusto <luizwagm@gmail.com>
2022-05-20 07:09:06 -0300

    feat: models and repositories begin

---
+++ app/Models/Orders.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Orders extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'invoice_code',
+        'id_branch_unit',
+        'guide_number',
+        'date',
+        'id_customer',
+        'claim_check',
+        'invoice',
+        'agreement_table',
+        'id_collaborator',
+        'observation',
+        'type',
+    ];
+
+    public function branchUnit(): BelongsTo
+    {
+        return $this->belongsTo(BranchUnits::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customers::class);
+    }
+
+    public function collaborator(): BelongsTo
+    {
+        return $this->belongsTo(Collaborators::class);
+    }
+}
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,12 +2,17 @@
 
 namespace App\Providers;
 
+use App\Repositories\BaseRepository;
+use App\Repositories\BaseRepositoryContract;
+use App\Repositories\Customer\CustomerRepository;
+use App\Repositories\Customer\CustomerRepositoryContract;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //$this->app->singleton(Contract::class, Repository::class);
+        $this->app->singleton(BaseRepositoryContract::class, BaseRepository::class);
+        $this->app->singleton(CustomerRepositoryContract::class, CustomerRepository::class);
     }
 }
